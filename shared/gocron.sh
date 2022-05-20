497668a3d39a6f2a2be01d1e3dcfcbf652745063
Alireza <bbis@cheen.cn>
2022-05-20 22:36:24 +0800

    Add files via upload

---
+++ shared/gocron.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="gocron"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
+export QNAP_QPKG=$QPKG_NAME
+
+case "$1" in
+  start)
+    ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+    if [ "$ENABLED" != "TRUE" ]; then
+        echo "$QPKG_NAME is disabled."
+        exit 1
+    fi
+	cd $QPKG_ROOT
+		./gocron web &
+		./gocron-node -allow-root &
+    ;;
+
+  stop)
+	killall -9 gocron
+	killall -9 gocron-node
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit 0
